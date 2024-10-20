ccda8c295c1c9d19c5137e9d2d2ee997e230ae72
tinebasilio <kristinebasilio28@gmail.com>
2024-10-20 18:30:05 +0800

    Add EmployeeService.cs

---
+++ Services/EmployeeService.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Module05Exercise01.Services;
+using Module05Exercise01.Model;
+using MySql.Data.MySqlClient;
+
+namespace Module05Exercise01.Services
+{
+    public class EmployeeService
+    {
+        private readonly string _connectionString;
+        public EmployeeService()
+        {
+            var dbService = new DatabaseConnectionService();
+            _connectionString = dbService.GetConnectionString();
+        }
+
+        public async Task<List<Employee>> GetAllEmployeesAsync()
+        {
+            var employeeService = new List<Employee>();
+            using (var conn = new MySqlConnection(_connectionString))
+            {
+                await conn.OpenAsync();
+
+                // Retrieve data
+                var cmd = new MySqlCommand("SELECT * FROM tblemployee", conn);
+
+                using (var reader = await cmd.ExecuteReaderAsync())
+                {
+                    while (await reader.ReadAsync())
+                    {
+                        employeeService.Add(new Employee
+                        {
+                            EmployeeID = reader.GetInt32("EmployeeID"),
+                            Name = reader.GetString("Name"),
+                            email = reader.GetString("email"),
+                            Address = reader.GetString("Address"),
+                            ContactNo = reader.GetString("ContactNo")
+                        });
+                    }
+                }
+            }
+            return employeeService;
+        }
+
+    }
+}
