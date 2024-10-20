03fabcd4f79687a9eab0094ac9a4eea7566fd833
tinebasilio <kristinebasilio28@gmail.com>
2024-10-20 18:42:36 +0800

    Add EmployeeViewModel.cs

---
+++ ViewModel/EmployeeViewModel.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Module05Exercise01.Model;
+using Module05Exercise01.Services;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Windows.Input;
+using System.Runtime.CompilerServices;
+
+namespace Module05Exercise01.ViewModel
+{
+    public class EmployeeViewModel : INotifyPropertyChanged
+    {
+        private readonly EmployeeService _employeeService;
+        public ObservableCollection<Employee> EmployeeList { get; set; }
+
+        private bool _isBusy;
+        public bool IsBusy
+        {
+            get => _isBusy;
+            set
+            {
+                _isBusy = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string _statusMessage;
+        public string StatusMessage
+        {
+            get => _statusMessage;
+            set
+            {
+                _statusMessage = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ICommand LoadDataCommand { get; }
+
+        //EmployeeViewModel Constructor
+
+        public EmployeeViewModel()
+        {
+            _employeeService = new EmployeeService();
+            EmployeeList = new ObservableCollection<Employee>();
+            LoadDataCommand = new Command(async () => await LoadData());
+
+            LoadData();
+        }
+
+        public async Task LoadData()
+        {
+            if (IsBusy) return;
+            IsBusy = true;
+            StatusMessage = "Loading employee data...";
+            try
+            {
+                var employees = await _employeeService.GetAllEmployeesAsync();
+                EmployeeList.Clear();
+                foreach (var employee in employees)
+                {
+                    EmployeeList.Add(employee);
+                }
+                StatusMessage = "Data loaded successfully!";
+            }
+            catch (Exception ex)
+            {
+                StatusMessage = $"Failed to load data: {ex.Message}";
+            }
+            finally
+            {
+                IsBusy = false;
+            }
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
