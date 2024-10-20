56cd626f7b92a73525786edcb5abfa81ed45acd4
tinebasilio <kristinebasilio28@gmail.com>
2024-10-20 18:32:10 +0800

    Add Employee.cs

---
+++ Model/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module05Exercise01.Model
+{
+    public class Employee
+    {
+        public int EmployeeID { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string email { get; set; }
+        public string ContactNo { get; set; }
+    }
+}
